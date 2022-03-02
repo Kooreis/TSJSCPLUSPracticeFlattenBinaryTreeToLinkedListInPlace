e8115fd4427506cc961dc110a0f3b09b1ef4bcd5
Kooreis <kooreis.official@gmail.com>
2022-03-02 05:34:22 -0600

    This update introduces the ListNode class, which is used to represent a node in a linked list. Each node has a value and a pointer to the next node in the list.

--- solution.ts
+++ solution.ts
@@ -1,10 +1,8 @@
-class TreeNode {
+class ListNode {
     val: number;
-    left: TreeNode | null;
-    right: TreeNode | null;
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
         this.val = (val === undefined ? 0 : val);
-        this.left = (left === undefined ? null : left);
-        this.right = (right === undefined ? null : right);
+        this.next = (next === undefined ? null : next);
     }
 }
